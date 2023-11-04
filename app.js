901bebf9d1ccbb3b505f8d7f8217807fe9aee490
Bhoami K Khona <bhoamikhona@gmail.com>
2023-11-04 12:24:55 +0530

    updating static directory path in app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
